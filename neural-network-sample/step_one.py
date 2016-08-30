85af3a0f15bf48df7acb48aea9ebc20175bc1351
datsuns <the.skeleton7@gmail.com>
2016-08-30 22:10:18 +0900

    add log diff --git a/neural-network-sample/step_one.py b/neural-network-sample/step_one.py index 1ef8115..d1f550e 100644 --- a/neural-network-sample/step_one.py +++ b/neural-network-sample/step_one.py @@ -18,3 +18,7 @@ training_data, validation_data, test_data = mnist_loader.load_data_wrapper()  net = network.Network([784, 30, 10])  net.SGD(training_data, 30, 10, 3.0, test_data=test_data) + +print "change hidden neurous 30 -> 100" +net = network.Network([784, 100, 10]) +net.SGD(training_data, 30, 10, 3.0, test_data=test_data)

--- neural-network-sample/step_one.py
+++ neural-network-sample/step_one.py
@@ -18,3 +18,7 @@ training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 
 net = network.Network([784, 30, 10])
 net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+
+print "change hidden neurous 30 -> 100"
+net = network.Network([784, 100, 10])
+net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
